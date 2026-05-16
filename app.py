712549f04490e1db00fc2c494605aa9ec438933b
keytapia <ktapia@fi.uba.ar>
2026-05-15 23:02:49 -0300

    remocion de carpetas y creacion de la base de app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+from routes.menu_routes import menu_bp
+from routes.reservas_routes import reservas_bp
+
+app = Flask(__name__)
+
+app.config.from_pyfile("config.py")
+
+app.register_blueprint(menu_bp)
+app.register_blueprint(reservas_bp)
+
+@app.route("/")
+def prueba():
+    return "todo ok"
+
+if __name__ = "--main__":
+  app.run(debug=True)
