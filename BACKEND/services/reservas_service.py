79fd843e1e5765f7d59d4dd7f5fca6e1c8f9dab8
keytapia <ktapia@fi.uba.ar>
2026-05-19 09:10:13 -0300

    add service from routes

--- BACKEND/services/reservas_service.py
+++ BACKEND/services/reservas_service.py
@@ -1,3 +1,35 @@
 from app.db import get_connection
 import re
 from datetime import datetime
+
+# Función para listar todas las reservas
+def listar_reservas():
+    con = get_connection()
+    try:
+        with con.cursor() as cursor:
+            cursor.execute("SELECT * FROM reservas")
+            reservas = cursor.fetchall()
+            return reservas
+    finally:        con.close()
+
+# Función para listar reservas por estado
+def listar_reservas_por_estado(estado):
+    con = get_connection()
+    try:
+        with con.cursor() as cursor:
+            cursor.execute("SELECT * FROM reservas WHERE estado = %s", (estado,))
+            reservas = cursor.fetchall()
+            return reservas
+    finally:
+        con.close()
+
+# Función para cancelar una reserva por id cambiando su estado a "cancelada"
+def cancelar_reserva(reserva_id):
+    con = get_connection()
+    try:
+        with con.cursor() as cursor:
+            cursor.execute("UPDATE reservas SET estado = 'cancelada' WHERE id = %s", (reserva_id,))
+            con.commit()
+            return cursor.rowcount > 0  # Devuelce True si se actualizó al menos una fila
+    finally:
+        con.close()
