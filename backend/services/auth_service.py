37e1d0028c5bf23158892a3b9417522661360fd5
Valeria Lichinizer <vlichinizer@fi.uba.ar>
2026-05-23 19:15:46 +0000

    Hago Endpoint de Login

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,3 +1,27 @@
 from flask import Flask, jsonify, Blueprint, request
 from db import get_connection
 from datetime import datetime
+
+
+# Función para iniciar sesión
+def login_usuario(email, contraseña):
+
+    con = get_connection()
+
+    try:
+        with con.cursor() as cursor:
+
+            sql = """
+                SELECT id, nombre, apellido, email, rol
+                FROM usuarios
+                WHERE email = %s AND contraseña = %s
+            """
+
+            cursor.execute(sql, (email, contraseña))
+
+            usuario = cursor.fetchone()
+
+            return usuario
+
+    finally:
+        con.close()
