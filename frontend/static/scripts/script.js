2f78020fa637e062946c4eae93e277404ffda245
keytapia <ktapia@fi.uba.ar>
2026-06-03 19:42:33 -0300

    cambio de la ruta en el script.js

--- frontend/static/scripts/script.js
+++ frontend/static/scripts/script.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         try {
-            const url = `/api/mesas-disponibles?fecha=${fecha}&horario=${horario}&cantidad_personas=${cantidadPersonas}`;
+            const url = `http://127.0.0.1:5000/api/mesas-disponibles?fecha=${fecha}&horario=${horario}&cantidad_personas=${cantidadPersonas}`;
 
             const respuesta = await fetch(url);
 
