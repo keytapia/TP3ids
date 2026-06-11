436ffa77c67c7259e9d57727f1fd08187aef72c6
scafed <fnscalise@gmail.com>
2026-06-10 22:40:03 -0300

    Cambios servicios

--- frontend/routes/admin/servicios.py
+++ frontend/routes/admin/servicios.py
@@ -16,14 +16,16 @@ def servicios():
     return render_template("admin/servicios.html", servicios=servicios_prueba)
 
 
-
-
 @servicios_admin_bp.route("/servicios/agregar", methods=["GET", "POST"])
 def agregar_servicio():
     return render_template("admin/agregar_servicio.html")
 
 
+@servicios_admin_bp.route("/servicios/editar/<int:id>", methods=["GET", "POST"])
+def editar_servicio(id):
+    return render_template("admin/editar_servicio.html", id=id)
 
-@servicios_admin_bp.route("/servicios/editar", methods=["GET", "POST"])
-def editar_servicio():
-    return render_template("admin/editar_servicio.html")
+
+@servicios_admin_bp.route("/servicios/eliminar/<int:id>", methods=["POST"])
+def eliminar_servicio(id):
+    pass
