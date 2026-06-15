40b9ef4eb2464648c36002cca4f2e2e0230baa62
keytapia <ktapia@fi.uba.ar>
2026-06-15 11:36:26 -0300

    agregando el services del front

--- frontend/services/resenas.py
+++ frontend/services/resenas.py
@@ -2,14 +2,16 @@ import requests
 
 API_BACKEND_URL = "http://127.0.0.1:5000"
 
-
 def obtener_resenas():
 
     try:
-
-        response = requests.get(f"{API_BACKEND_URL}/api/admin/resenas", timeout=10)
+        response = requests.get(
+            f"{API_BACKEND_URL}/api/resenas",
+            timeout=10
+        )
 
         if response.status_code == 200:
+
             return {
                 "ok": True,
                 "data": response.json()
@@ -17,14 +19,13 @@ def obtener_resenas():
 
         return {
             "ok": False,
-            "data": [],
             "error": response.text
         }
 
     except requests.exceptions.ConnectionError:
+        
         return {
             "ok": False,
-            "data": [],
             "error": "No se pudo conectar con el backend."
         }
 
@@ -51,3 +52,76 @@ def eliminar_resena(id):
 
     except requests.exceptions.ConnectionError:
         return False
+    
+def obtener_resena_por_id(id_resena):
+
+    try:
+
+        response = requests.get(
+            f"{API_BACKEND_URL}/api/resenas/{id_resena}",
+            timeout=10
+        )
+
+        if response.status_code == 200:
+
+            return {
+                "ok": True,
+                "data": response.json()
+            }
+
+        return {
+            "ok": False,
+            "error": response.text
+        }
+
+    except requests.exceptions.ConnectionError:
+
+        return {
+            "ok": False,
+            "error": "No se pudo conectar al backend"
+        }
+    
+    except requests.exceptions.Timeout:
+
+        return {
+            "ok": False,
+            "error": "No se pudo conectar al backend"
+        }
+
+
+def crear_resena(reserva_id, nombre, apellido, comentario, puntuacion):
+
+    datos = {
+        "reserva_id": reserva_id,
+        "nombre": nombre,
+        "apellido": apellido,
+        "comentario": comentario,
+        "puntuacion": puntuacion
+    }
+
+    try:
+
+        response = requests.post(
+            f"{API_BACKEND_URL}/api/resenas",
+            json=datos,
+            timeout=10
+        )
+
+        if response.status_code == 201:
+
+            return {
+                "ok": True,
+                "data": response.json()
+            }
+
+        return {
+            "ok": False,
+            "error": response.text
+        }
+
+    except requests.exceptions.ConnectionError:
+
+        return {
+            "ok": False,
+            "error": "No se pudo conectar al backend"
+        }
