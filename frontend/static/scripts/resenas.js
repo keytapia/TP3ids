eed2e496a5623d1779f88bf25b50355fd2d089a1
keytapia <ktapia@fi.uba.ar>
2026-06-15 17:26:55 -0300

    reseñas public ya funciona por complet, falta implementar la funcion de mostar y ocultar

---
+++ frontend/static/scripts/resenas.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const slider = document.querySelector("resenas-slider");
+    const nextBtn = document.querySelector(".next-btn");
+    const prevBtn = document.querySelector(".prev-btn");
+
+    if (!slider || !nextBtn || !prevBtn) return;
+
+    const card = document.querySelector(".resena-card");
+
+    if (!card) return;
+
+    const scrollAmount = card.offsetWidth +25;
+
+    nextBtn.addEventListener("click", () => {
+        slider.scrollBy([
+            left: scrollAmount,
+            behavior: "smooth"
+        ])
+    })
+
+    prevBtn.addEventListener("click", () => {
+        slider.scrollBy([
+            left: -scrollAmount,
+            behavior: "smooth"
+        ])
+    })
+})
