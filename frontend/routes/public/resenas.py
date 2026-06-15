a47a80cd7e03cc90b66f4d82ba1ada0d028814dd
keytapia <ktapia@fi.uba.ar>
2026-06-15 15:46:18 -0300

    ya se ven las reseñas, pero no esta terminado, falta paginar y faltan cosas del css

--- frontend/routes/public/resenas.py
+++ frontend/routes/public/resenas.py
@@ -1,8 +1,60 @@
-from flask import Flask, render_template, Blueprint
+from flask import Flask, render_template, Blueprint, request, redirect, url_for, flash
+
+from services.resenas import (
+    obtener_resenas,
+    crear_resena
+)
 
 resenas_bp = Blueprint('resenas', __name__)
 
 # Reseñas
-@resenas_bp.route("/resenas")
+@resenas_bp.route("/resenas", methods=["GET"])
 def resenas():
-    return render_template("public/resenas.html")
+
+    resultado = obtener_resenas()
+
+    listar_resenas = []
+
+    if resultado.get("ok"):
+        listar_resenas = resultado.get("data", [])
+
+    else:
+        flash("No se pudieron cargar las reseñas")
+
+    return render_template(
+        "public/resenas.html",
+        resenas=listar_resenas
+    )
+
+@resenas_bp.route("/crear-resena", methods=["GET", "POST"])
+def crear_resena_form():
+
+    if request.method == "POST":
+
+        reserva_id=request.form.get("reserva_id")
+        nombre=request.form.get("nombre")
+        apellido=request.form.get("apellido")
+        comentario=request.form.get("comentario")
+        puntuacion=request.form.get("puntuacion")
+
+        resultado = crear_resena(
+            reserva_id=reserva_id,
+            nombre=nombre,
+            apellido=apellido,
+            comentario=comentario,
+            puntuacion=puntuacion
+        )
+
+        if resultado.get("ok"):
+            flash("Reseña creada exitosamente")
+            return redirect(url_for("resenas.resenas"))
+        
+        flash("No se pudo crear la reseña")
+        return redirect(url_for("resenas.crear_resena_form"))
+    
+    reserva_id = request.args.get("reserva_id")
+
+    return render_template (
+        "public/crear_resena.html",
+        reserva_id=reserva_id
+    )
